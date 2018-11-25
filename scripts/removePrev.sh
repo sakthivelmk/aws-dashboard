1f53725a7ddbf7748c2d1cb4e3e807b765550144
sakthi <shakthivelmk@gmail.com>
2018-11-25 18:01:41 +0400

    added appspec file and shell scripts

---
+++ scripts/removePrev.sh
@@ -0,0 +1,3 @@
+docker stop aws-dashboard
+docker rm aws-dashboard
+docker rmi 042524953207.dkr.ecr.eu-west-1.amazonaws.com/aws-dashboard:latest
---
+++ scripts/runImg.sh
@@ -0,0 +1,1 @@
+docker run --name aws-dashboard -d -p 8080:80 042524953207.dkr.ecr.eu-west-1.amazonaws.com/aws-dashboard:latest
