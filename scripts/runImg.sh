fc77940fddbf4fa333e4027429f18d1b2da13ec2
sakthi <shakthivelmk@gmail.com>
2018-11-26 17:17:16 +0400

    edited docker login command

--- scripts/runImg.sh
+++ scripts/runImg.sh
@@ -1,2 +1,2 @@
-eval $(aws ecr get-login --region us-east-1)
+eval $(aws ecr get-login --region eu-west-1)
 docker run --name aws-dashboard -d -p 8080:80 042524953207.dkr.ecr.eu-west-1.amazonaws.com/aws-dashboard:latest
